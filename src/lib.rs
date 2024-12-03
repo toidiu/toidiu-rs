9c90d5917d335c29783e2a127aa073130a06bf89
Apoorv Kothari <apoorv@toidiu.com>
2024-12-03 11:10:31 -0800

    feature tag in docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
+// Tag docs with the required platform and features.
+//
+// https://doc.rust-lang.org/rustdoc/unstable-features.html
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(
+    docsrs,
+    feature(doc_auto_cfg),
+    feature(doc_cfg_hide),
+    doc(cfg_hide(doc))
+)]
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+#[cfg(feature = "feature_1")]
+pub fn feature_1() {}
+
+#[cfg_attr(docsrs, doc(cfg(feature = "override_tag_feature_1")))]
+#[cfg(feature = "feature_1")]
+pub fn feature_1_override() {}
 
 #[cfg(test)]
 mod tests {
